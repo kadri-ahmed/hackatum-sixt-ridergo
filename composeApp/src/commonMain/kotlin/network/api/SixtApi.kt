75684f259559701265517733c26fc57c01088882
Ahmed Kadri <kadri.ahmedbaha@gmail.com>
2025-11-22 09:54:29 +0100

    Sixt API implemented

--- composeApp/src/commonMain/kotlin/network/api/SixtApi.kt
+++ composeApp/src/commonMain/kotlin/network/api/SixtApi.kt
@@ -3,16 +3,23 @@ package network.api
 import dto.AvailableVehiclesDto
 import dto.BookingDto
 import dto.CreateBookingDto
-import dto.ProtectionPackageDto
+import dto.ProtectionPackagesDto
 import utils.NetworkError
 import utils.Result
 
 interface SixtApi {
+
+    // Booking Management
     suspend fun createBooking(): Result<CreateBookingDto, NetworkError>
     suspend fun getBooking(bookingId: String): Result<BookingDto, NetworkError>
     suspend fun getAvailableVehicles(bookingId: String): Result<AvailableVehiclesDto, NetworkError>
+    suspend fun getAvailableProtectionPackages(bookingId: String): Result<ProtectionPackagesDto, NetworkError>
+    suspend fun assignVehicleToBooking(bookingId: String, vehicleId: String): Result<BookingDto, NetworkError>
+    suspend fun assignProtectionPackageToBooking(bookingId: String, packageId: String): Result<BookingDto, NetworkError>
+    suspend fun completeBooking(bookingId: String): Result<BookingDto, NetworkError>
 
-    suspend fun getAvailableProtectionPackages(bookingId: String): Result<ProtectionPackageDto, NetworkError>
-
-
+    // Car Lock/Unlock API
+    suspend fun lockCar(): Result<Unit, NetworkError>
+    suspend fun unlockCar(): Result<Unit, NetworkError>
+    suspend fun blinkCar(): Result<Unit, NetworkError>
 }
--- composeApp/src/commonMain/kotlin/network/api/SixtApiImpl.kt
+++ composeApp/src/commonMain/kotlin/network/api/SixtApiImpl.kt
@@ -1,16 +1,15 @@
 package network.api
 
-import io.ktor.client.*
-import io.ktor.client.call.body
-import io.ktor.client.request.*
-import io.ktor.http.ContentType
-import io.ktor.http.contentType
-import io.ktor.util.network.UnresolvedAddressException
-import kotlinx.serialization.SerializationException
 import dto.AvailableVehiclesDto
 import dto.BookingDto
 import dto.CreateBookingDto
-import dto.ProtectionPackageDto
+import dto.ProtectionPackagesDto
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.util.network.*
+import kotlinx.serialization.SerializationException
 import utils.NetworkError
 import utils.Result
 
@@ -94,7 +93,191 @@ class SixtApiImpl(private val client: HttpClient) : SixtApi {
         }
     }
 
-    override suspend fun getAvailableProtectionPackages(bookingId: String): Result<ProtectionPackageDto, NetworkError> {
-        TODO("Not yet implemented")
+    override suspend fun getAvailableProtectionPackages(bookingId: String): Result<ProtectionPackagesDto, NetworkError> {
+        val response = try {
+            client.get(
+                "$SIXT_API_URL/api/booking/$bookingId/vehicles"
+            ) {
+                contentType(ContentType.Application.Json)
+            }
+        } catch (e: UnresolvedAddressException) {
+            return Result.Error(NetworkError.NO_INTERNET)
+        } catch (e: SerializationException) {
+            return Result.Error(NetworkError.SERIALIZATION)
+        }
+        return when(response.status.value) {
+            in 200..299 -> {
+                val availableProtectionPackages = response.body<ProtectionPackagesDto>()
+                Result.Success(availableProtectionPackages)
+            }
+            401 -> Result.Error(NetworkError.UNAUTHORIZED)
+            409 -> Result.Error(NetworkError.CONFLICT)
+            408 -> Result.Error(NetworkError.REQUEST_TIMEOUT)
+            413 -> Result.Error(NetworkError.PAYLOAD_TOO_LARGE)
+            in 500..599 -> Result.Error(NetworkError.SERVER_ERROR)
+            else -> Result.Error(NetworkError.UNKNOWN)
+        }
+    }
+
+    override suspend fun assignVehicleToBooking(
+        bookingId: String,
+        vehicleId: String
+    ): Result<BookingDto, NetworkError> {
+        val response = try {
+            client.post(
+                "$SIXT_API_URL/api/booking/$bookingId/vehicles/$vehicleId"
+            ) {
+                contentType(ContentType.Application.Json)
+            }
+        } catch (e: UnresolvedAddressException) {
+            return Result.Error(NetworkError.NO_INTERNET)
+        } catch (e: SerializationException) {
+            return Result.Error(NetworkError.SERIALIZATION)
+        }
+        return when(response.status.value) {
+            in 200..299 -> {
+                val updatedBooking = response.body<BookingDto>()
+                Result.Success(updatedBooking)
+            }
+            401 -> Result.Error(NetworkError.UNAUTHORIZED)
+            409 -> Result.Error(NetworkError.CONFLICT)
+            408 -> Result.Error(NetworkError.REQUEST_TIMEOUT)
+            413 -> Result.Error(NetworkError.PAYLOAD_TOO_LARGE)
+            in 500..599 -> Result.Error(NetworkError.SERVER_ERROR)
+            else -> Result.Error(NetworkError.UNKNOWN)
+        }
+    }
+
+    override suspend fun assignProtectionPackageToBooking(
+        bookingId: String,
+        packageId: String
+    ): Result<BookingDto, NetworkError> {
+        val response = try {
+            client.post(
+                "$SIXT_API_URL/api/booking/$bookingId/protections/$packageId"
+            ) {
+                contentType(ContentType.Application.Json)
+            }
+        } catch (e: UnresolvedAddressException) {
+            return Result.Error(NetworkError.NO_INTERNET)
+        } catch (e: SerializationException) {
+            return Result.Error(NetworkError.SERIALIZATION)
+        }
+        return when(response.status.value) {
+            in 200..299 -> {
+                val updatedBooking = response.body<BookingDto>()
+                Result.Success(updatedBooking)
+            }
+            401 -> Result.Error(NetworkError.UNAUTHORIZED)
+            409 -> Result.Error(NetworkError.CONFLICT)
+            408 -> Result.Error(NetworkError.REQUEST_TIMEOUT)
+            413 -> Result.Error(NetworkError.PAYLOAD_TOO_LARGE)
+            in 500..599 -> Result.Error(NetworkError.SERVER_ERROR)
+            else -> Result.Error(NetworkError.UNKNOWN)
+        }
+    }
+
+    override suspend fun completeBooking(bookingId: String): Result<BookingDto, NetworkError> {
+        val response = try {
+            client.post(
+                "$SIXT_API_URL/api/booking/$bookingId/complete"
+            ) {
+                contentType(ContentType.Application.Json)
+            }
+        } catch (e: UnresolvedAddressException) {
+            return Result.Error(NetworkError.NO_INTERNET)
+        } catch (e: SerializationException) {
+            return Result.Error(NetworkError.SERIALIZATION)
+        }
+        return when(response.status.value) {
+            in 200..299 -> {
+                val completedBooking = response.body<BookingDto>()
+                Result.Success(completedBooking)
+            }
+            401 -> Result.Error(NetworkError.UNAUTHORIZED)
+            409 -> Result.Error(NetworkError.CONFLICT)
+            408 -> Result.Error(NetworkError.REQUEST_TIMEOUT)
+            413 -> Result.Error(NetworkError.PAYLOAD_TOO_LARGE)
+            in 500..599 -> Result.Error(NetworkError.SERVER_ERROR)
+            else -> Result.Error(NetworkError.UNKNOWN)
+        }
+    }
+
+    override suspend fun lockCar(): Result<Unit, NetworkError> {
+        val response = try {
+            client.post(
+                "$SIXT_API_URL/api/car/lock"
+            ) {
+                contentType(ContentType.Application.Json)
+            }
+        } catch (e: UnresolvedAddressException) {
+            return Result.Error(NetworkError.NO_INTERNET)
+        } catch (e: SerializationException) {
+            return Result.Error(NetworkError.SERIALIZATION)
+        }
+        return when(response.status.value) {
+            in 200..299 -> {
+                val carLockSuccess = response.body<Unit>()
+                Result.Success(carLockSuccess)
+            }
+            401 -> Result.Error(NetworkError.UNAUTHORIZED)
+            409 -> Result.Error(NetworkError.CONFLICT)
+            408 -> Result.Error(NetworkError.REQUEST_TIMEOUT)
+            413 -> Result.Error(NetworkError.PAYLOAD_TOO_LARGE)
+            in 500..599 -> Result.Error(NetworkError.SERVER_ERROR)
+            else -> Result.Error(NetworkError.UNKNOWN)
+        }
+    }
+
+    override suspend fun unlockCar(): Result<Unit, NetworkError> {
+        val response = try {
+            client.post(
+                "$SIXT_API_URL/api/car/unlock"
+            ) {
+                contentType(ContentType.Application.Json)
+            }
+        } catch (e: UnresolvedAddressException) {
+            return Result.Error(NetworkError.NO_INTERNET)
+        } catch (e: SerializationException) {
+            return Result.Error(NetworkError.SERIALIZATION)
+        }
+        return when(response.status.value) {
+            in 200..299 -> {
+                val carLockSuccess = response.body<Unit>()
+                Result.Success(carLockSuccess)
+            }
+            401 -> Result.Error(NetworkError.UNAUTHORIZED)
+            409 -> Result.Error(NetworkError.CONFLICT)
+            408 -> Result.Error(NetworkError.REQUEST_TIMEOUT)
+            413 -> Result.Error(NetworkError.PAYLOAD_TOO_LARGE)
+            in 500..599 -> Result.Error(NetworkError.SERVER_ERROR)
+            else -> Result.Error(NetworkError.UNKNOWN)
+        }
+    }
+
+    override suspend fun blinkCar(): Result<Unit, NetworkError> {
+        val response = try {
+            client.post(
+                "$SIXT_API_URL/api/car/blink"
+            ) {
+                contentType(ContentType.Application.Json)
+            }
+        } catch (e: UnresolvedAddressException) {
+            return Result.Error(NetworkError.NO_INTERNET)
+        } catch (e: SerializationException) {
+            return Result.Error(NetworkError.SERIALIZATION)
+        }
+        return when(response.status.value) {
+            in 200..299 -> {
+                val carLockSuccess = response.body<Unit>()
+                Result.Success(carLockSuccess)
+            }
+            401 -> Result.Error(NetworkError.UNAUTHORIZED)
+            409 -> Result.Error(NetworkError.CONFLICT)
+            408 -> Result.Error(NetworkError.REQUEST_TIMEOUT)
+            413 -> Result.Error(NetworkError.PAYLOAD_TOO_LARGE)
+            in 500..599 -> Result.Error(NetworkError.SERVER_ERROR)
+            else -> Result.Error(NetworkError.UNKNOWN)
+        }
     }
 }
